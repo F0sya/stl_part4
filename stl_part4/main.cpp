8e02a1f119219b3e47fe643a66b10cc599630ba0
F0sya <nikitachmyria@gmail.com>
2024-11-23 11:38:13 +0200

    task 2 is completed

--- stl_part4/main.cpp
+++ stl_part4/main.cpp
@@ -1,106 +1,54 @@
 #include <iostream>
-#include <map>
-#include <Windows.h>
-#include <vector>
 #include <string>
+#include <map>
+#include <sstream>
+#include <locale>
+#include <Windows.h>
+using namespace std; 
 
-using namespace std;
-
-double average_num(vector<int> numbers) {
-	double sum = 0;
-	for (int i = 0; i < numbers.size(); i++) {
-		sum += numbers[i];
-	}
-	return sum / numbers.size();
-}
-
-void print(string name, vector<int> numbers) {
-	cout << "	≤м'€:" << name << endl;
-	cout << "	–езультати:";
-	for (int i = 0; i < numbers.size(); i++) {
-		cout << numbers[i] << " ";
-	}
-	cout << endl;
+void print(map<string,int> mp) {
+    for (const auto& pair : mp) {
+        cout << pair.first << " Ч " << pair.second << '\n';
+    }
 }
 
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int choice;
-	int num;
-	double sum = 0;
-	string name;
-	vector<int> numbers;
-
-	map<string, vector<int>> students;
-
-	
-	while (true) {
-		cout << "\n----------MENU------------\n";
-		cout << "1.Add to map new student" << endl;
-		cout << "2.Add new mark to a student" << endl;
-		cout << "3.Print average mark of student" << endl;
-		cout << "4.Exit" << endl;
-		cout << "\n----------MENU------------\n";
-		
-		cin >> choice;
-
-		cin.ignore();
-
-		switch (choice) {
-
-		case 1:
-			cout << "¬вед≥ть ≥м'€: ";
-			getline(cin, name);
-			cout << "¬вед≥ть оц≥нки (через проб≥л): ";
-
-			while (cin >> num) {
-				numbers.push_back(num);
-				if (cin.get() == '\n') {
-					break;
-				}
-			}
-			cout << "ƒодано студента:" << endl;
-			print(name, numbers);
-
-			students.insert({ name,numbers });
-			break;
-		case 2: {
-			string found_name;
-			int new_mark;
-			vector<int> arr;
-			cout << "Enter a name:"; getline(cin, found_name);
-			if (students.find(found_name) != students.end()) {
-				cout << "Enter a new mark:"; cin >> new_mark;
-				arr = students.find(found_name)->second;
-				arr.push_back(new_mark);
-				students[found_name] = arr;
-				print(found_name, arr);
-			}
-			else {
-				cout << "No occurencies" << endl;
-			}
-			break;
-			}
-		case 3: {
-			string found_name;
-			vector<int> arr;
-			cout << "Enter a name:";
-			getline(cin, found_name);
-			if (students.find(found_name) != students.end()) {
-				cout << name << " : середн≥й бал = " << average_num(students.find(found_name)->second);
-			}
-			else {
-				cout << "No occurencies" << endl;
-			}
-			break;
-			}
-		case 4:
-			exit(0);
-		}
-	}
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string text;
+    cout << "¬вед≥ть текст: ";
+    getline(cin, text);
 
 
-	return 0;
+
+    string word;
+    size_t pos = 0;
+    map<string, int> mp;
+    while ((pos = text.find(" ")) != string::npos) {
+        word = text.substr(0, pos);
+        mp.insert({ word, 0 });
+        for (auto i : mp) {
+            if (i.first == word) {
+                int temp = i.second + 1;
+                mp.erase(i.first);
+                mp.insert({word,temp});
+            }
+        }
+        text.erase(0, pos + 1);
+    }
+    mp.insert({ text, 0 });
+    for (auto i : mp) {
+        if (i.first == text) {
+            int temp = i.second + 1;
+            mp.erase(i.first);
+            mp.insert({ text,temp });
+        }
+    }
+
+    for (const auto& pair : mp) {
+        cout << pair.first << " Ч " << pair.second << '\n';
+    }
+
+    return 0;
 }
